49ae3fa92d2865029d979e69a46aceb73148ae26
shravanEWW <shravan.kushwaha@excellentwebworld.in>
2023-07-03 12:20:02 +0530

    Token added

--- app/src/main/java/com/example/mynotescompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/mynotescompose/ui/theme/Color.kt
@@ -11,10 +11,3 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-//
-//- name: Show APK
-//uses: ncipollo/release-action@v1.12.0
-//with:
-//artifacts: "app/build/outputs/apk/**/*.apk"
-//token: ${{ secrets.CICD }}
-//tag: v1.0.0
