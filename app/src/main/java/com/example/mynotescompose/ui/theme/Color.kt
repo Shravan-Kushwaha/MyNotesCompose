c48a12b3fe1d56ef897fa3ecec1fec5f0c149361
shravanEWW <shravan.kushwaha@excellentwebworld.in>
2023-07-03 12:12:25 +0530

    init

---
+++ app/src/main/java/com/example/mynotescompose/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.mynotescompose.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val Pink800 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+//
+//- name: Show APK
+//uses: ncipollo/release-action@v1.12.0
+//with:
+//artifacts: "app/build/outputs/apk/**/*.apk"
+//token: ${{ secrets.CICD }}
+//tag: v1.0.0
